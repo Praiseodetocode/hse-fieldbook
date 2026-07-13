212f3baa3ff68f48a762f7ebfb5aea71dd980590
Praiseodetocode <edorepraise00@gmail.com>
2026-07-13 18:01:22 +0100

    Pushing clean file and CSS

--- main.jsx
+++ main.jsx
@@ -6,6 +6,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import LandingPage from './LandingPage.jsx'
+import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LandingPage/>
